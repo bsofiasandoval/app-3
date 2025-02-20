7d1c375a9f1fc0ab9f78e6d7b8ad622e7baefe67
Brenda Sofia Sandoval <bsofiasandoval2@gmail.com>
2025-02-20 12:42:46 -0600

    class work

---
+++ src/app/page1/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, { useState } from 'react';
+
+const Page1: React.FC = () => {
+    const [color, setColor] = useState<string>('white');
+
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-white text-black gap-4">
+            <div
+                style={{
+                    width: '200px',
+                    height: '200px',
+                    backgroundColor: color,
+                    marginBottom: '20px',
+                }}
+            ></div>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+                <button
+                    style={{ backgroundColor: 'red', padding: '10px' }}
+                    onClick={() => setColor('red')}
+                >
+                    Red
+                </button>
+                <button
+                    style={{ backgroundColor: 'yellow', padding: '10px' }}
+                    onClick={() => setColor('yellow')}
+                >
+                    Yellow
+                </button>
+                <button
+                    style={{ backgroundColor: 'green', padding: '10px' }}
+                    onClick={() => setColor('green')}
+                >
+                    Green
+                </button>
+                <button
+                    style={{ backgroundColor: 'blue', padding: '10px' }}
+                    onClick={() => setColor('blue')}
+                >
+                    Blue
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Page1;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Navigation() {
+    return (
+       <header className="w-full flex justify-between items-center p-4 text-white">
+            
+        </header>
+    );
+}
